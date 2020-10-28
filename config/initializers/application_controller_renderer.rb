74c9eb35e3239cd687f98ed84f044a8f6066bf2a
vagrant <vagrant@localhost.localdomain>
2020-10-28 06:00:16 +0000

    user周り作成

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'a0b8eb65e28ac1f927c009792daed06b1d73d2b7cd0e9d344c0a4d7b56ac269a42b37d0fdbbbd9863c695dab85d52700831012e2e5469f9c445c9af37b3eea4a'
