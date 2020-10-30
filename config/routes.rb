7461eeede11bf8df510ac8f63d309e54ec2dac70
vagrant <vagrant@localhost.localdomain>
2020-10-30 14:54:02 +0000

    検索機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'search/search'
   devise_for :users
   root 'home#top'
   get 'home/about'
   resources :users,only: [:show,:index,:edit,:update]
   resources :books,only: [:index,:create,:show,:edit,:update,:destroy]
+  get '/search', to: 'search#search'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
